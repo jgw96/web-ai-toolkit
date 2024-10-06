e2bf3feba65b3384786c9c7d5d900c0b51105b8a
jgw96 <jgw9617@gmail.com>
2024-10-05 22:53:21 -0700

    better tests with vitetest + improvements to test.html

--- src/services/summarization/summarization.ts
+++ src/services/summarization/summarization.ts
@@ -3,13 +3,18 @@ import { pipeline, env } from '@huggingface/transformers';
 let summarizer: any = undefined;
 
 export async function runSummarizer(text: string, model: string = "Xenova/distilbart-cnn-6-6") {
-    return new Promise(async (resolve) => {
-        if (!summarizer) {
-            await loadSummarizer(model);
-        };
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!summarizer) {
+                await loadSummarizer(model);
+            };
 
-        const out = await summarizer(text);
-        resolve(out);
+            const out = await summarizer(text);
+            resolve(out);
+        }
+        catch (err) {
+            reject(err);
+        }
     });
 }
 
