1d36b20263827a4fe0b4b5b28d4aa10e77639968
Justin Willis <jgw9617@gmail.com>
2025-06-22 05:00:26 +0000

    text to speech wasm support

--- src/services/text-to-speech/tts.ts
+++ src/services/text-to-speech/tts.ts
@@ -1,4 +1,5 @@
 import { KokoroTTS } from "kokoro-js";
+import { webGPUCheck } from "../../utils";
 
 let synthesizer: any = undefined;
 
@@ -9,7 +10,6 @@ export async function runSynthesizer(text: string) {
         await loadSynthesizer();
       };
       const out = await synthesizer.generate(text, {
-        // Use `tts.list_voices()` to list all available voices
         voice: "af_heart",
       });
 
@@ -23,10 +23,12 @@ export async function runSynthesizer(text: string) {
 
 export async function loadSynthesizer() {
   return new Promise(async (resolve) => {
+    const gpuCheck = await webGPUCheck();
+
     const model_id = "onnx-community/Kokoro-82M-v1.0-ONNX";
     const tts = await KokoroTTS.from_pretrained(model_id, {
-      dtype: "fp32", // Options: "fp32", "fp16", "q8", "q4", "q4f16"
-      device: "webgpu", // Options: "wasm", "webgpu" (web) or "cpu" (node). If using "webgpu", we recommend using dtype="fp32".
+      dtype: gpuCheck ? "fp32" : "q8",
+      device: gpuCheck ? "webgpu" : "wasm",
     });
 
     synthesizer = tts;
